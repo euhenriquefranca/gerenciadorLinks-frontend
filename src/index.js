21161a4e627f81cdbb4bbce036ebdd9780af59f1
euhenriquefranca <euhenriquefranca@gmail.com>
2020-07-03 14:14:03 -0300

    Added UI Kit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import './main.scss';
+import './styles/main.scss';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
