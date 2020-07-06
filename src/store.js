55660e4e55e5f1a52fae148754ffac90edbc1378
euhenriquefranca <euhenriquefranca@gmail.com>
2020-07-06 16:38:39 -0300

    Sign up implemented

--- src/store.js
+++ src/store.js
@@ -2,10 +2,13 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import ReduxPromise from 'redux-promise';
 
 import signInReducer from './screens/SignIn/SignInReducer';
+import signUpReducer from './screens/SignUp/SignUpReducer';
 
 const reducers = combineReducers({
   signIn: signInReducer,
+  signUp: signUpReducer,
 });
+
 
 const store = createStore(reducers, applyMiddleware(ReduxPromise));
 
